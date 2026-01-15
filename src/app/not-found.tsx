c6b7621b05ab8cbab8100cdf47693a1c9d9e356b
Priyashu Tiwari <priyanshu.tiwari@neosoftmail.com>
2026-01-15 12:09:41 +0530

    fixed css

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@
 
 export default function NotFound() {
   return (
-    <div className="min-h-screen flex items-center justify-cente px-4">
+    <div className="min-h-screen flex items-center justify-center px-4">
       <div className="max-w-md w-full text-center bg-[#1c1c1c] text-white p-8 rounded-2xl shadow-lg">
         <h1 className="text-7xl font-bold">404</h1>
 
