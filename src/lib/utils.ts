ba5f2e5b0dd0257191d8bebeb144c4fa5c78c06b
Priyashu Tiwari <priyanshu.tiwari@neosoftmail.com>
2026-01-15 11:53:03 +0530

    removed-unused shadcn components dependencies

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,9 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-import { Message } from "@/generated/prisma";
+import { Message } from "@/generated/prisma/client";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
 
 export function normalizeMessage(message: Message) {
